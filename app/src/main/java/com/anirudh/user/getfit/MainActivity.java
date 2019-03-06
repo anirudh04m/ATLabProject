a20045733770d1ff6df7a89907f178654e9cfc34
Anirudh Maheshwari <anirudh04m@gmail.com>
2019-03-06 20:37:52 +0530

    reset button added

--- app/src/main/java/com/anirudh/user/getfit/MainActivity.java
+++ app/src/main/java/com/anirudh/user/getfit/MainActivity.java
@@ -7,6 +7,8 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +19,8 @@ public class MainActivity extends Activity implements SensorEventListener {
      boolean running = false  ;
 
      SensorManager sensor ;
+        float cvalue = 0;
+     Button reset ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +30,17 @@ public class MainActivity extends Activity implements SensorEventListener {
 
         sensor = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
+        reset = (Button) findViewById(R.id.reset);
+
+        reset.setOnClickListener(new View.OnClickListener() {
+                                     @Override
+                                     public void onClick(View v) {
+                                         //sensor.unregisterListener(MainActivity.this);
+                                         cvalue = Float.parseFloat(count.getText().toString());
+                                         count.setText ("0");
+                                     }
+                                 }
+        );
 
 
     }
@@ -52,7 +67,7 @@ public class MainActivity extends Activity implements SensorEventListener {
     @Override
     public void onSensorChanged(SensorEvent event) {
         if (running){
-            count.setText(String.valueOf(event.values[0]));
+            count.setText(String.valueOf(event.values[0] - cvalue));
         }
     }
 
